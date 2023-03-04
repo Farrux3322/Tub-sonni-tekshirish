fdbd5e6ff2b03d87967d55a15968b8bc4122f11c
Farrux Madaminov +998937373322 <madaminovfarrux3322@gmail.com>
2023-03-04 13:59:22 +0500

    tub son

---
+++ main.py
@@ -0,0 +1,18 @@
+def isPrime(son:int):
+    if son==2:
+        return True
+    if son%2==0 or son<2:
+        return False
+    for i in range(3,son//2+1):
+        if son%i==0:
+            return False
+    return True
+
+
+
+
+son=int(input("Son kiriting: "))
+if isPrime(son):
+    print("Tub son")
+else:
+    print("Tub son emas")
